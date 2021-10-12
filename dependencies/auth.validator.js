3aa7e14226459082a7c7b84902c1cf6f0690d883
Maks-Storoshschuk <maksstoroschuk@gmail.com>
2021-10-12 21:32:04 +0300

    спроба 1

--- dependencies/auth.validator.js
+++ dependencies/auth.validator.js
@@ -11,7 +11,7 @@ const authValidator = Joi.object({
     password: Joi
         .string()
         .trim()
-        .allow(passwordRegExp)
+        .regex(passwordRegExp)
         .required()
 });
 
