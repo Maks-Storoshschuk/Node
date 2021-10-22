53897c70fa3c56fb7baebaba861ee55237a12d28
Maks-Storoshschuk <maksstoroschuk@gmail.com>
2021-10-22 17:44:04 +0300

    pzdc

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const cors = require('cors');
 require('dotenv').config();
 
 const {config} = require('./config');
@@ -11,6 +12,7 @@ const app = express();
 
 mongoose.connect(config.MongoConnectUrl);
 
+app.use(cors({origin: 'http://localhost:3000'}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
