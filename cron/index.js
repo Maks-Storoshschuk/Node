fec40f11c1a27d7c6fa4742761ea48448221d7d0
Maks-Storoshschuk <maksstoroschuk@gmail.com>
2021-10-27 13:06:59 +0300

    lesson8_1

--- cron/index.js
+++
@@ -1,9 +0,0 @@
-const cron = require('node-cron');
-
-const removeoldTokens = require('../cron/removeOldTokens');
-
-module.exports = () => {
-    cron.schedule('*/10 * * * * *', async () => {
-        await removeoldTokens();
-    });
-};
