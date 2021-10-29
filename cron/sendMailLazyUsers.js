fd68772c147837791fd173241183253e13dc3fef
Maks-Storoshschuk <maksstoroschuk@gmail.com>
2021-10-29 21:25:30 +0300

    lesson11_ще не готово

--- cron/sendMailLazyUsers.js
+++ cron/sendMailLazyUsers.js
@@ -15,10 +15,18 @@ module.exports = async () => {
     const lazyUsers = await O_auth
         .find({updatedAt: {$lt: tenDays}});
 
+    const emailArray = [];
+
     lazyUsers.forEach(lazyUser => {
         const email = lazyUser.user_id.email;
 
-        fs.readFile(path.join(__dirname,'mailSent.txt'),((err, data) => {
+        emailArray.push(email);
+    });
+
+    const setEmail = new Set(emailArray);
+
+    setEmail.forEach(email => {
+        fs.readFile(path.join(__dirname, 'mailSent.txt'), ((err, data) => {
 
             if (err) {
                 return;
@@ -27,12 +35,13 @@ module.exports = async () => {
             const lazyUserArray = data.toString().split(',');
 
             if (!lazyUserArray.includes(email)) {
-                fs.appendFile(path.join(__dirname,'mailSent.txt'),`${email},`,(error)=>{
+                fs.appendFile(path.join(__dirname, 'mailSent.txt'), `${email},`, (error) => {
 
-                    if (error){
-                        return;
+                    if (error) {
+                        return (error);
                     }
                     sendMail(email);
+                    Promise.allSettled(email);
                 });
             }
         }));
@@ -41,10 +50,8 @@ module.exports = async () => {
 
 async function sendMail(email) {
     try {
-
         await emailService.sendMail(email, constants.hi, {userName: 'Мамина черешня'});
-
     } catch (e) {
-        console.log(e);
+        return(e);
     }
 }
